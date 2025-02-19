2c3a2920fee68a50300a08640c68b606948bd09c
Yuma Kishi <obake2ai@gmail.com>
2025-02-19 18:29:02 +0900

    add auto

--- config/config.py
+++ config/config.py
@@ -8,42 +8,42 @@ PI_CONFIG = {
     # 各転送先の設定（ここに最大20件程度追加可能）
     'settings': [
         {
-            'host': 'zero2wh01.local', #zero2wh04.local
+            'host': 'zero2wh01',
             'target_dir': './share/12x6',
             'chain_length': 12,
             'parallel': 6,
             'idx': 1,
         },
         {
-            'host': 'zero2wh02.local', #zero2wh04.local
+            'host': 'zero2wh02',
             'target_dir': './share/12x6',
             'chain_length': 12,
             'parallel': 6,
             'idx': 2,
         },
         {
-            'host': 'zero2wh03.local', #zero2wh04.local
+            'host': 'zero2wh03',
             'target_dir': './share/12x6',
             'chain_length': 12,
             'parallel': 6,
             'idx': 3,
         },
         {
-            'host': 'zero2wh04.local', #zero2wh04.local
+            'host': 'zero2wh04',
             'target_dir': './share/12x6',
             'chain_length': 12,
             'parallel': 6,
             'idx': 4,
         },
         {
-            'host': 'zero2wh05.local', #zero2wh04.local
+            'host': 'zero2wh05',
             'target_dir': './share/12x6',
             'chain_length': 12,
             'parallel': 6,
             'idx': 5,
         },
         {
-            'host': 'zero2wh06.local', #zero2wh04.local
+            'host': 'zero2wh06',
             'target_dir': './share/12x6',
             'chain_length': 12,
             'parallel': 6,
